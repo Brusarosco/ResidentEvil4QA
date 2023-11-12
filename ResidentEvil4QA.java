851677d7a1543861d12964019fcd18571c023a34
Guilherme Brusarosco Franco <71615014+Brusarosco@users.noreply.github.com>
2023-11-12 11:12:45 -0300

    Update ResidentEvil4QA.java
    Foram realizada algumas alterações na centralização das perguntas, e o e modificado o frame.dispose(), para que a janela não fosse fechada e aberta, sempre que fizesse uma nova pergunta

--- ResidentEvil4QA.java
+++ ResidentEvil4QA.java
@@ -1,4 +1,5 @@
 package com.mycompany.residentevil4qa;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -23,20 +24,20 @@ public class ResidentEvil4QA {
     }
 
     private static void createAndShowGUI() {
-        if (frame != null) {
-            frame.dispose();
+        if (frame == null) {
+            frame = new JFrame("Resident Evil 4 Quiz");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            mainPanel = new JPanel();
+            mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+            frame.add(mainPanel);
+        } else {
+            mainPanel.removeAll();
         }
 
-        frame = new JFrame("Resident Evil 4 Quiz");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        frame.add(mainPanel);
-
         questionPanel = new JPanel();
-        questionPanel.setLayout(new BoxLayout(questionPanel, BoxLayout.Y_AXIS));
-        questionLabel = new JLabel("<html><div style='text-align: center;'>" + questions[currentQuestion] + "</div></html>");
+        questionPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        questionLabel = new JLabel("<html>" + questions[currentQuestion] + "</html>");
         questionLabel.setFont(new Font("Arial", Font.BOLD, 14));
         questionPanel.add(questionLabel);
         mainPanel.add(questionPanel);
@@ -44,7 +45,7 @@ public class ResidentEvil4QA {
         buttonGroup = new ButtonGroup();
         answerButtons = new JRadioButton[answers[currentQuestion].length];
         JPanel answerPanel = new JPanel();
-        answerPanel.setLayout(new GridLayout(0, 1));
+        answerPanel.setLayout(new GridLayout(5, 5));
 
         for (int i = 0; i < answers[currentQuestion].length; i++) {
             answerButtons[i] = new JRadioButton(answers[currentQuestion][i]);
@@ -61,11 +62,11 @@ public class ResidentEvil4QA {
         buttonPanel.add(submitButton);
         mainPanel.add(buttonPanel);
 
-        correctAnswersTextArea = new JTextArea(10, 40); // Configura o tamanho da caixa de texto
-        correctAnswersTextArea.setEditable(false); // Impede a edição do texto
+        correctAnswersTextArea = new JTextArea(20, 40);
+        correctAnswersTextArea.setEditable(false);
         mainPanel.add(correctAnswersTextArea);
 
-        frame.setPreferredSize(new Dimension(600, 400)); // Aumenta a altura da janela
+        frame.setPreferredSize(new Dimension(1000, 1000));
         frame.pack();
         frame.setVisible(true);
     }
@@ -104,19 +105,19 @@ public class ResidentEvil4QA {
     }
 
     private static String[] questions = {
-        "Qual é o nome do protagonista de Resident Evil 4?",
-        "Qual é a organização que sequestrou a filha do presidente?",
-        "Qual é a localização geográfica principal em Resident Evil 4?",
-        "Quem é o antagonista principal em Resident Evil 4?",
-        "Qual é a principal ameaça biológica em Resident Evil 4?"
+            "Qual é o nome do protagonista de Resident Evil 4?",
+            "Qual é a organização que sequestrou a filha do presidente?",
+            "Qual é a localização geográfica principal em Resident Evil 4?",
+            "Quem é o antagonista principal em Resident Evil 4?",
+            "Qual é a principal ameaça biológica em Resident Evil 4?"
     };
 
     private static String[][] answers = {
-        {"Chris Redfield", "Leon Kennedy", "Jill Valentine", "Claire Redfield"},
-        {"S.T.A.R.S.", "Umbrella Corporation", "Los Illuminados", "BSAA"},
-        {"Raccoon City", "Arklay Mountains", "Ilha Rockfort", "Espanha rural"},
-        {"Albert Wesker", "William Birkin", "Ada Wong", "Osmund Saddler"},
-        {"Zumbis", "Vírus T", "Las Plagas", "Plantas mutantes"}
+            {"Chris Redfield", "Leon Kennedy", "Jill Valentine", "Claire Redfield"},
+            {"S.T.A.R.S.", "Umbrella Corporation", "Los Illuminados", "BSAA"},
+            {"Raccoon City", "Arklay Mountains", "Ilha Rockfort", "Espanha rural"},
+            {"Albert Wesker", "William Birkin", "Ada Wong", "Osmund Saddler"},
+            {"Zumbis", "Vírus T", "Las Plagas", "Plantas mutantes"}
     };
 
     private static int[] correctAnswers = {1, 2, 3, 3, 2};
